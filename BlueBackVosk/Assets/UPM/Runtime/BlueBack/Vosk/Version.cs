bf0473f11380e27f03964bf19138bac9801a5b79
blueback <blueback28@gmail.com>
2022-08-10 04:26:12 +0900

    修正 : README.md / 0.0.2

--- BlueBackVosk/Assets/UPM/Runtime/BlueBack/Vosk/Version.cs
+++ BlueBackVosk/Assets/UPM/Runtime/BlueBack/Vosk/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Vosk
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.1";
+		public const string packageversion = "0.0.2";
 
 		/** GetPackageVersion
 		*/
